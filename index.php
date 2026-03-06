c449d20611b1fedf506fce090d3513a96d7928f9
govert@muze.nl <govert@muze.nl>
2026-03-06 16:29:51 +0100

    wip stuff, wrong door

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<?php include 'generated.html'; ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+
+    <h1>
+        <?php
+            echo "Hello, World";
+        ?>
+    </h1>
+
+    <p>
+        <?php
+            echo "The teach asked me to echo gibberish text within a paragraph tag.";
+        ?>
+    </p>
+
+</body>
+</html>
